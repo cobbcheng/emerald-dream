18d543236906179332aaf4539d514569a8ce4191
chengkebin <chengkebin1@jwzg.com>
2022-03-26 12:36:57 +0800

    test

--- client/src/store/sku.js
+++ client/src/store/sku.js
@@ -8,58 +8,32 @@ class Sku {
   }
 
   @observable list = [];
+  @observable stockList = [];
 
   @action.bound
   getBoxList(id) {
-    this.skuId = id;
-    this.callFunction({
-      // 云函数名称
-      name: 'getSkuDetail',
-      // 传给云函数的参数
-      data: {
-        id,
-      },
-    }).then((res) => {
-      console.log(res);
-    });
-  }
-
-  @action.bound
-  async getBox() {
-    console.log('getBox', this);
-    const res = await this.db.runTransaction(this.runTr);
-    return res;
-    // try {
-    //   const res = await this.db.runTransaction(this.runTr);
-    //   return res;
-    // } catch (e) {
-    //   return {
-    //     boxes: [],
-    //     stocks: [],
-    //   };
-    // }
-  }
-
-  @action.bound
-  async runTr(tr) {
-    const id = this.skuId;
-    const boxes = await tr
+    this.db
       .collection('box')
       .where({
         sku_id: id,
       })
-      .get();
+      .get()
+      .then((res) => {
+        this.list = res.data;
+      });
+  }
 
-    const map = boxes.data.map((box) => box._id);
-    const _ = this.db.command;
-    const stocks = await tr.collection('stock').where({
-      box_id: _.in(map),
-    });
-
-    return {
-      boxes,
-      stocks,
-    };
+  @action.bound
+  getStockList(boxId) {
+    this.db
+      .collection('stock')
+      .where({
+        box_id: boxId,
+      })
+      .get()
+      .then((res) => {
+        this.stockList = res.data;
+      });
   }
 }
 
