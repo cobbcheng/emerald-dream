a53855bfcb7af1957015e882152cb42b2d1c0994
chengkebin <kebincheng@yeah.net>
2022-04-17 00:01:46 +0800

    pack page

--- client/src/pages/sku/index.jsx
+++ client/src/pages/sku/index.jsx
@@ -23,15 +23,39 @@ export default class Sku extends Component {
   render() {
     const { list } = this.props.store.sku;
     return (
-      <>
-        <View>
-          {list.map((item) => (
-            <Button onClick={() => this.getPrize(item._id)} key={item._id}>
-              第{item.number}箱
-            </Button>
-          ))}
+      // <>
+      //   <View>
+      //     {list.map((item) => (
+      //       <Button onClick={() => this.getPrize(item._id)} key={item._id}>
+      //         第{item.number}箱
+      //       </Button>
+      //     ))}
+      //   </View>
+      // </>
+      <View className="sku">
+        <View className="header">
+          <View className="header-pic"></View>
+          <View className="btn">
+            <View className="btn-record"></View>
+            <View className="btn-price">98</View>
+          </View>
         </View>
-      </>
+        <View className="line"></View>
+        <View className="tab">
+          <View className="tab-btn tab-pre"></View>
+          <View className="tab-info">
+            第 <Text className="tab-info-number">1</Text>/246 箱 &nbsp; 赏品余量&nbsp;
+            <Text className="tab-info-number">33</Text>/80
+          </View>
+          <View className="tab-btn tab-next"></View>
+        </View>
+
+        <View className="list"></View>
+
+        <View className="chong"></View>
+
+        <View className="bottom"></View>
+      </View>
     );
   }
 }
