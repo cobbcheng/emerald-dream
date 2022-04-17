d0d5e60679a83bf85749a6fe6e84320fc566fc38
chengkebin <kebincheng@yeah.net>
2022-04-17 18:32:18 +0800

    pack page

--- client/src/pages/sku/index.jsx
+++ client/src/pages/sku/index.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { View, Text, Button } from '@tarojs/components';
+import { View, Text, Image } from '@tarojs/components';
 import { observer, inject } from 'mobx-react';
 import { getCurrentInstance } from '@tarojs/taro';
 import './index.less';
@@ -50,11 +50,31 @@ export default class Sku extends Component {
           <View className="tab-btn tab-next"></View>
         </View>
 
-        <View className="list"></View>
+        <View className="list">
+          <View className="item">
+            <View className="item-pic">
+              <Image
+                className="item-pic-detail"
+                src="https://7969-yifanshang-8g5d7nxddf660e3e-1310253199.tcb.qcloud.la/cloudbase-cms/upload/2022-04-04/2jj3ubjaougzm5nglxa70p3bvxfpum7w_.jpeg?sign=0616876bc47705c73d08cf1895ce2c0b&t=1649042184"
+              ></Image>
+              <View className="item-left">12/24</View>
+            </View>
+            <View className="item-info">A赏 大龙山一日游</View>
+          </View>
+        </View>
 
-        <View className="chong"></View>
+        <View className="fixed">
+          <View className="chong">
+            <View className="chong-item chong-1"></View>
+            <View className="chong-item chong-3"></View>
+            <View className="chong-item chong-5"></View>
+          </View>
 
-        <View className="bottom"></View>
+          <View className="bottom">
+            <View className="bottom-change"></View>
+            <View className="bottom-all"></View>
+          </View>
+        </View>
       </View>
     );
   }
